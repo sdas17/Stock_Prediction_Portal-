53fddf433d12f69817f06c9379bdcb97a4c7939c
sdas17 <subhamkvgms112@gmail.com>
2026-02-15 21:22:10 +0530

    main page completed -subham

--- frontend_portal/my-react-app/src/component/Header.jsx
+++ frontend_portal/my-react-app/src/component/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='text-light'>Header</div>
+    <nav className='navbar container pt-3 pb-3 align-items-start'>
+     <a className='navbar-brand text-light' href='#'>Stock Predication Portal</a>
+     <div>
+        <a className='btn btn-outline-info' href='#'>Login</a>
+        &nbsp;
+                <a className='btn btn-info' href='#'>Login</a>
+
+     </div>
+    </nav>
   )
 }
 
